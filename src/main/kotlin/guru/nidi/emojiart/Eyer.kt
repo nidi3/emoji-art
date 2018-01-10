7cbc884264d085b1a0883ccf947b044f65c16550
Stefan Niederhauser <stefan.niederhauser.3@post.ch>
2018-01-10 12:40:44 +0100

    add eyer to shorten waiting time

---
+++ src/main/kotlin/guru/nidi/emojiart/Eyer.kt
@@ -0,0 +1,78 @@
+/*
+ * Copyright © 2017 Stefan Niederhauser (nidin@gmx.ch)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package guru.nidi.emojiart
+
+class Eyer {
+    private var thread: Thread? = null
+    @Volatile private var running = true
+
+    fun start() {
+        if (thread == null) {
+            thread = Thread {
+                while (running) {
+                    eyes("(o)(o)", 800, 1200)
+                    if (Math.random() < .2) {
+                        bigEyes()
+                    } else if (Math.random() < .4) {
+                        oneEye()
+                    } else {
+                        blink()
+                    }
+                }
+                thread = null
+            }.apply {
+                isDaemon = true
+                start()
+            }
+        }
+    }
+
+    fun stop() {
+        running = false
+    }
+
+    private fun bigEyes() {
+        eyes("(O)(O)", 300, 500)
+        eyes("(o)(o)", 100, 200)
+        eyes("(O)(O)", 300, 500)
+    }
+
+    private fun oneEye() {
+        val s = if (Math.random() < .5) "(-)(o)(_)(O)" else "(o)(-)(O)(_)"
+        eyes(s.substring(0, 6), 50, 150)
+        eyes(s.substring(6, 12), 500, 800)
+    }
+
+    private fun blink() {
+        val s = if (Math.random() < .3) "(-)(-)(_)(_)" else if (Math.random() < .66) "(-)(o)(_)(o)" else "(o)(-)(o)(_)"
+        eyes(s.substring(0, 6), 20, 50)
+        eyes(s.substring(6, 12), 20, 50)
+    }
+
+    private fun eyes(s: String, min: Int, max: Int) {
+        print(s + "\u001b[6D")
+        System.out.flush()
+        sleep(min, max)
+    }
+
+    private fun sleep(min: Int, max: Int) {
+        try {
+            Thread.sleep((Math.random() * (max - min) + min).toLong())
+        } catch (e: InterruptedException) {
+            //ignore
+        }
+    }
+}
